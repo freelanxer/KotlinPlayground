b9a7201f8ec9a8b856b1f9527ef99553b207983c
Daniel <kuanshenwang@gmail.com>
2023-10-24 23:39:10 +0800

    Add Retrofit

---
+++ app/src/main/java/com/freelanxer/kotlinplayground/network/RetrofitManager.kt
@@ -0,0 +1,41 @@
+package com.freelanxer.kotlinplayground.network
+
+import com.freelanxer.kotlinplayground.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+class RetrofitManager {
+    private val mockBaseUrl = "https://mock.apifox.cn/m1/3472734-0-default/"
+    private val okHttpClient: OkHttpClient
+    private val retrofit: Retrofit
+
+    init {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level =
+            if (isDebug())
+                HttpLoggingInterceptor.Level.BODY
+            else
+                HttpLoggingInterceptor.Level.NONE
+
+        okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .retryOnConnectionFailure(true)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(mockBaseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    fun isDebug(): Boolean =
+        BuildConfig.BUILD_TYPE.contentEquals("debug")
+
+}
